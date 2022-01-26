ec82d4126b00dfa986c37d7837d5c1f150270dc4
Dima <youdima70@gmail.com>
2022-01-26 11:14:33 +0200

    Refactoring

--- app/src/main/java/ua/dimaevseenko/format_player/fragment/player/navigation/home/ChannelsFragment.kt
+++ app/src/main/java/ua/dimaevseenko/format_player/fragment/player/navigation/home/ChannelsFragment.kt
@@ -4,29 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TabHost
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.tabs.TabItem
 import com.google.android.material.tabs.TabLayout
-import ua.dimaevseenko.format_player.R
-import ua.dimaevseenko.format_player.appComponent
+import ua.dimaevseenko.format_player.*
 import ua.dimaevseenko.format_player.databinding.FragmentChannelsBinding
 import ua.dimaevseenko.format_player.fragment.player.navigation.AnimatedFragment
-import ua.dimaevseenko.format_player.network.Server
 import ua.dimaevseenko.format_player.network.result.PlaylistResult
-import ua.dimaevseenko.format_player.viewmodel.PlaylistViewModel
+import ua.dimaevseenko.format_player.replaceFragment
 import javax.inject.Inject
 
-class ChannelsFragment @Inject constructor(): AnimatedFragment(), Server.Listener<PlaylistResult> {
+class ChannelsFragment @Inject constructor(): AnimatedFragment() {
 
     companion object{
         const val TAG = "ChannelsFragment"
     }
 
     private lateinit var binding: FragmentChannelsBinding
-
-    @Inject lateinit var playlistViewModelFactory: PlaylistViewModel.Factory
-    private lateinit var playlistViewModel: PlaylistViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentChannelsBinding.bind(inflater.inflate(R.layout.fragment_channels, container, false))
@@ -38,29 +31,7 @@ class ChannelsFragment @Inject constructor(): AnimatedFragment(), Server.Listene
         if(savedInstanceState == null)
             animateStartX()
 
-        playlistViewModel = ViewModelProvider(requireActivity(), playlistViewModelFactory).get(PlaylistViewModel::class.java)
-        playlistViewModel.listener = this
-        playlistViewModel.getGenres()
-
         binding.backCard.setOnClickListener { dismiss() }
-    }
-
-    override fun onResponse(result: PlaylistResult) {
-        binding.channelsGenresTabLayout.addTab(binding.channelsGenresTabLayout.newTab().apply {
-            text = "Все"
-            id = 0
-        })
-
-        result.genres.forEach {
-            binding.channelsGenresTabLayout.addTab(binding.channelsGenresTabLayout.newTab().apply {
-                text = it.name
-                id = it.id.toInt()
-            })
-        }
-    }
-
-    override fun onFailure(t: Throwable) {
-
     }
 
     fun dismiss(){
